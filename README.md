298dadea567992e7a4e801e4b8049ae5fb41b1d0
Johnnie <99084912+johnnie-610@users.noreply.github.com>
2023-12-02 14:13:03 +0300

    final

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Rock-Paper-Scissor-GAME
+
+[Photo by Pavel Danilyuk](https://www.pexels.com/photo/people-playing-rock-paper-scissor-game-8111311/)
+
+[Video by KoolShooters](https://www.pexels.com/video/close-up-of-skaters-playing-rock-paper-scissors-9724550/)
+
+[Video by ANTONI SHKRABA production](https://www.pexels.com/video/friends-playing-rock-paper-scissors-8684427/)
--- index.js
+++ index.js
@@ -24,16 +24,16 @@ function getComputerChoice() {
  */
 function getPlayerChoice() {
     let choice = prompt("'Rock','Paper', 'Scissors'; What do you choose?")
-    if (choice.toLowerCase().trim() != ("rock" || "paper" || "scissors")){
-        choice = prompt("Enter either of the three, 'Rock', 'Paper', 'Scissors'?")
-        if (choice.toLowerCase().trim() != ("rock" || "paper" || "scissors")) {
-            alert("Wrongdoing!! Refresh!")
-            return
-        }
-    }
-    return choice
 
+    if (choice.toLowerCase().trim() == "rock" || choice.toLowerCase().trim() == "paper" || choice.toLowerCase().trim() == "scissors") {
+        return choice
+    } else {
+        alert("Please enter 'Rock', 'Paper', or 'Scissors'.")
+        return getPlayerChoice()
+    }
 }
+
+
 
 
 /**
@@ -43,23 +43,81 @@ function getPlayerChoice() {
  * @param {string} computerSelection - the computer's choice of Rock, Paper, or Scissors
  * @return {string} the result of the round (e.g. "YOU WIN! Rock beats Scissors")
  */
-function playRound(playerSelection, computerSelection) {
-    ps = playerSelection.toLowerCase().trim()
-    cs = computerSelection.toLowerCase().trim()
-    if (ps === "rock" && cs === "scissors") {
-        return "YOU WIN! Rock beats Scissors"
-    } else if (ps === "scissors" && cs === "rock") {
-        return "YOU LOSE! Rock beats Scissors"
-    } else if (ps === "rock" && cs === "paper") {
-        return "YOU LOSE! Paper beats Rock"
-    } else if (ps === "paper" && cs === "rock") {
-        return "YOU WIN! Paper beats Rock"
-    } else if (ps === "scissors" && cs === "paper") {
-        return "YOU WIN! Scissors beats Paper"
-    } else if (ps === "paper" && cs === "scissors") {
-        return "YOU LOSE! Scissors beats Paper"
-    } else if (ps === cs) {
-        return "DRAW"
+function playRound() {
+    let ps = getPlayerChoice().toLowerCase().trim()
+    let cs = getComputerChoice().toLowerCase().trim()
+
+    // in a tie, round is replayed
+    if (ps === cs) {
+        alert("Round is a tie")
+        // replay round
+        playRound();
+    } else if ((ps === "rock" && cs === "scissors") || (ps === "paper" && cs === "rock") || (ps === "scissors" && cs === "paper")) {
+        return `YOU WIN! ${ps} beats ${cs}`
+    }else {
+        return `YOU LOSE! ${cs} beats ${ps}`
+    }
+
+
+}
+
+/**
+ * Plays the game until a certain number of rounds are reached. Keeps track of player and computer scores
+ * and displays the results after each round. Alerts the final score and game over message at the end.
+ *
+ * @param {number} rounds - The number of rounds to play.
+ * @return {undefined} No return value.
+ */
+function game() {
+    const rounds = 5
+    let playerScore = 0
+    let computerScore = 0
+    let currentRound = 1
+    while (currentRound <= rounds) {
+        let result = playRound()
+
+        if (result.includes("WIN")) {
+            playerScore++
+        } else if (result.includes("LOSE")) {
+            computerScore++
+        }
+        if (playerScore === 3 || computerScore === 3) {
+            break
+        }
+        alert(`Round ${currentRound}: ${result}\nScore: You ${playerScore} - ${computerScore} Computer`)
+        currentRound++
+    }
+    if (currentRound <= rounds) {
+        if (playerScore > computerScore) {
+            alert(`YOU WIN! Final Score: You ${playerScore} - ${computerScore} Computer`)
+            alert("Game Over! Thanks for playing!")
+            playAgain()
+
+        } else {
+            alert(`YOU LOSE! Final Score: You ${playerScore} - ${computerScore} Computer`)
+            alert("Game Over! Thanks for playing!")
+            playAgain()
+        }
+    } else {
+        if (playerScore > computerScore) {
+            alert(`YOU WIN! Final Score: You ${playerScore} - ${computerScore} Computer`)
+            alert("Game Over! Thanks for playing!")
+            playAgain()
+        } else {
+            alert(`YOU LOSE! Final Score: You ${playerScore} - ${computerScore} Computer`)
+            alert("Game Over! Thanks for playing!")
+            playAgain()
+        }
+
     }
 }
 
+function playAgain() {
+    let restart = confirm("Would you like to play again?")
+    if (restart) {
+        location.reload()
+    }
+}
+
+const button = document.querySelector("#play-button")
+button.addEventListener("click", game)
